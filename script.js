0a9b9fecaebaa6c53383c86442a6ada616b8ef3f
jyclave <jyclave@gmail.com>
2024-12-11 17:56:23 +0100

    liste for

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log("Hello World")
+const listeMots = ["Cachalot", "Pétunia", "Serviette"]
+
+let score = 0
+
+for (let i = 0; i < listeMots.length; i++) {
+    let motUtilisateur = prompt('Entrez le mot : ' + listeMots[i])
+    if (motUtilisateur === listeMots[i]) {
+        score++
+    }
+
+}
+
+
+console.log("Votre score est de " + score + " sur " + listeMots.length)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
