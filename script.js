66eccf08bfe47575ec7f892a069787f828833b91
jyclave <jyclave@gmail.com>
2024-12-12 09:17:46 +0100

    les boucles

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
+// Déclaration des tableaux contenant les listes des mots proposés à l'utilisateur
 const listeMots = ["Cachalot", "Pétunia", "Serviette"]
+const listePhrases = ["Pas de panique !", "La vie, l'univers et le reste", "Merci pour le poisson"]
 
 let score = 0
 
-for (let i = 0; i < listeMots.length; i++) {
-    let motUtilisateur = prompt('Entrez le mot : ' + listeMots[i])
-    if (motUtilisateur === listeMots[i]) {
-        score++
-    }
-
+// Déclaration de la variable contenant le choix de l'utilisateur
+let choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")
+// Tant que l'utilisateur n'a pas saisi "mots" ou "phrases", on lui redemande de saisir un choix
+while (choix !== "mots" && choix !== "phrases") {
+    choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")
 }
 
+if (choix === "mots") {
+    // On parcourt le tableau des mots
+    for (let i = 0; i < listeMots.length; i++) {
+        // On demande à l'utilisateur de saisir le mot correspondant à l'indice i
+        let motUtilisateur = prompt("Entrez le mot : " + listeMots[i])
+        if (motUtilisateur === listeMots[i]) {
+            // Si le mot saisi par l'utilisateur est correct, on incrémente le score
+            score++
+        }
+    }
+    console.log("Votre score est de " + score + " sur " + listeMots.length)
+} else {
+    // On parcourt le tableau des phrases
+    for (let i = 0; i < listePhrases.length; i++) {
+        // On demande à l'utilisateur de saisir la phrase correspondant à l'indice i
+        let phraseUtilisateur = prompt("Entrez la phrase : " + listePhrases[i])
+        if (phraseUtilisateur === listePhrases[i]) {
+            // Si la phrase saisi par l'utilisateur est correct, on incrémente le score
+            score++
+        }
+    }
+    console.log("Votre score est de " + score + " sur " + listePhrases.length)
+}
 
-console.log("Votre score est de " + score + " sur " + listeMots.length)
 
 
 
